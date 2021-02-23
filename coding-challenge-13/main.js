99533caed18d903f6b1f9b73d49e49015b10cacd
Amjad Elhassan <amjad_elhassan2005@yahoo.com>
2021-02-22 21:00:37 -0500

    coding challenge 13 update 2

--- coding-challenge-13/main.js
+++ coding-challenge-13/main.js
@@ -121,3 +121,69 @@ function isItSpecial(x) {
         }
     }
 }
+
+//best practices
+function isPalindrome(number){   
+    return number.toString().length > 2 && 
+            /^(.?)(.?)(.?)(.?)(.?).?\5\4\3\2\1$/.test(number);
+  }
+  
+  function isSequentialAsc(number){    
+    return number.toString().length > 2 &&
+            '1234567890'.indexOf(number) >= 0;  
+  }
+  
+  function isSequentialDesc(number){
+    return number.toString().length > 2 && 
+            '9876543210'.indexOf(number) >= 0;  
+  }
+  
+  function isAwesomePhrase(number, awesomePhrases){
+    return awesomePhrases.some( function(n){ return n==number; } );
+  }
+  
+  function isFollowedByZero(number){ 
+    return  number % 100 == 0 || 
+            number % 1000 == 0 || 
+            number % 10000 == 0;
+  }
+  
+  function isSameDigits(number){
+    return /^(\d)\1{2,4}$/.test(number);
+  }
+  
+  function isInteresting(number, awesomePhrases) {
+    
+    var isInteresting = 0;  
+    
+    for(var i=0; i<3 && !isInteresting && number >= 98 ; ++i, number++){             
+      isInteresting = isPalindrome(number) ||
+                    isSequentialAsc(number) || 
+                    isSequentialDesc(number) ||
+                    isAwesomePhrase(number, awesomePhrases) ||
+                    isFollowedByZero(number) ||
+                    isSameDigits(number) ? (i==0?2:1) : 0;
+    
+    } 
+    return isInteresting;  
+  }
+
+  function bestPractice2(number, awesomePhrases) {
+    var tests = [
+      function(n) { return /^\d00+$/.test(n); },
+      function(n) { return /^(\d)\1+$/.test(n); },
+      function(n) { return RegExp(n).test(1234567890); },
+      function(n) { return RegExp(n).test(9876543210); },
+      function(n) { return n + '' == (n + '').split('').reverse().join(''); },
+      function(n) { return awesomePhrases.some(function(p) { return p == n; }); }
+    ];
+    
+    var interesting = 0;
+    tests.some(function(test) {
+      if (number > 99 && test(number))
+        return interesting = 2;
+      else if ((number > 98 && test(number + 1)) || (number > 97 && test(number + 2)))
+        interesting = 1;
+    });
+    return interesting;
+  }
